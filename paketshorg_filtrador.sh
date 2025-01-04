4471ab81291b65da81ea2be10c879ddd600b71aa
ArtEze <passymas@gmail.com>
2025-01-04 20:51:04 -0300

    Add files via upload

--- paketshorg_filtrador.sh
+++ paketshorg_filtrador.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$#" -ne 4 ]; then
-    echo "Uso: $0 <nombre_del_archivo.so> filtro1 filtro2 filtro3"
-    echo "Ejemplo: $0 libXext.so.6 cooker lib64 x86_64"
+    echo "Uso: $0 filtro1 filtro2 filtro3 <nombre_del_archivo.so>"
+    echo "Ejemplo: $0 cooker lib64 x86_64 libXext.so.6"
     exit 1
 fi
 
-paketshorg.sh "$1" | grep -i "$2" | grep -i "$3" | grep -i "$4"
+paketshorg.sh "$4" | grep -i "$1" | grep -i "$2" | grep -i "$3"
